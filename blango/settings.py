2d61a0bf5e7e50dd47f01a83d185b584df309c1b
nadé kamali <kamali.nade@gmail.com>
2023-10-31 18:54:53 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
         "allauth.socialaccount.providers.google",
         "rest_framework",
         "rest_framework.authtoken",
+        "drf_yasg",
     ]
     REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
@@ -225,6 +226,12 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
+    }
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
